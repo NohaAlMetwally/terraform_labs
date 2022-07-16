d771826d93cfc459e94e83180447730a43169fb8
noha <noha@ubuntu21.lab.local>
2022-07-17 01:56:40 +0200

    terraform lab2

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mybucket-tf"
+    bucket         = "new-tf-bucket17"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "dynamodb_statelock"
